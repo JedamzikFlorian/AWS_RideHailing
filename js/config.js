d9aab14ffe69a389d7a14cf38e47b94760611f34
JedamzikFlorian <62438667+JedamzikFlorian@users.noreply.github.com>
2025-05-11 14:25:20 +0200

    Update config.js
    Add user pool and client ids as well as region

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_1ibjTTOM9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1g6cor7ups74ls08mn230t1pd0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
